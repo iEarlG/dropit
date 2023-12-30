40fe93c9478cbcf7ff3d7bc729c54aba699a9264
Earl A. Villapaz <jasonburning16@gmail.com>
2023-12-30 20:48:46 +0800

    3/5: Navigation

--- components/Nav/Navigation.tsx
+++ components/Nav/Navigation.tsx
@@ -1,26 +1,45 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { ProductCategories } from "@/lib";
 
 import { NavigationItem } from "@/components/Nav/NavigationItem";
+import { useOnClickOutside } from "@/hooks/useClickOutside";
 
 export const Navigation = () => {
-    const [activeIndex, setActiveIndex] = useState<null | number>(null);
+    const [isActive, setIsActive] = useState<null | number>(null);
+    const navRef = useRef<HTMLDivElement | null>(null);
 
-    const isAnyOpen = activeIndex !== null;
+    useEffect(() => {
+      const handleEsc = (e: KeyboardEvent) => {
+        if (e.key === "Escape") {
+          setIsActive(null);
+        }
+      }
+      document.addEventListener("keydown", handleEsc);
+
+      return () => {
+        document.removeEventListener("keydown", handleEsc);
+      }
+    }, []);
+    
+
+    const isAnyOpen = isActive !== null;
+
+    useOnClickOutside(navRef, () => setIsActive(null));
+
     return (
-        <div className="flex h-full gap-4">
+        <div className="flex h-full gap-4" ref={navRef}>
             {ProductCategories.map((category, i) => {
                 const handleOpen = () => {
-                    if (activeIndex === i) {
-                        setActiveIndex(null);
+                    if (isActive === i) {
+                        setIsActive(null);
                     } else {
-                        setActiveIndex(i);
+                        setIsActive(i);
                     }
                 };
-                const isOpen = i === activeIndex;
+                const isOpen = i === isActive;
 
                 return (
                     <NavigationItem 
