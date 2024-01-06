d78c5c5a95d39782b6ca1502f906576b1023e3d6
Earl A. Villapaz <jasonburning16@gmail.com>
2024-01-06 18:04:01 +0800

    6: Server Side

--- lib/PayloadConfig.ts
+++ lib/PayloadConfig.ts
@@ -2,6 +2,7 @@ import { buildConfig } from "payload/config";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import path from "path";
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
@@ -10,11 +11,21 @@ export default buildConfig({
         admin: "/sell",
     },
     admin: {
-        bundler: webpackBundler()
+        bundler: webpackBundler(),
+        meta: {
+            titleSuffix: "- Drop IT",
+            favicon: "/favicon.ico",
+            ogImage: "/thumbnail.jpg",
+        }
+    },
+    rateLimit: {
+        max: 2000
     },
     editor: slateEditor({}),
     db: mongooseAdapter({
         url: process.env.MONGODB_URL!
     }),
-
-})
+    typescript: {
+        outputFile: path.resolve(__dirname, "payload-types.ts"),
+    }
+});
